4698a12e0eee940685e7bd8b42ef48d083c15687
jacano <jacanosalado@gmail.com>
2026-09-15 20:44:38 +0200

    chore: drop the build time from the sitemap index
    The lastmod of the sitemap index changed on every build, so it said nothing. The value is optional, so drop it.

--- src/pages/sitemap-index.xml.ts
+++ src/pages/sitemap-index.xml.ts
@@ -2,17 +2,14 @@ import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = () => {
   const baseUrl = 'https://jacano.github.io';
-  const currentDate = new Date().toISOString();
 
-  let xml = `<?xml version="1.0" encoding="UTF-8"?>
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
     <loc>${baseUrl}/sitemap-pages.xml</loc>
-    <lastmod>${currentDate}</lastmod>
   </sitemap>
   <sitemap>
     <loc>${baseUrl}/sitemap-blog.xml</loc>
-    <lastmod>${currentDate}</lastmod>
   </sitemap>
 </sitemapindex>`;
 
